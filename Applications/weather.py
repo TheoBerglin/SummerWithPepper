d36e8229e01f5fa08d62b5ef18bc949ff2c1e24e
AdamLiberda <adam.liberda@ericsson.com>
2019-07-11 14:35:41 +0200

    Added a dict for the icons + some html parsing etc.

--- Applications/weather.py
+++ Applications/weather.py
@@ -1,5 +1,5 @@
 import forecastio
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from bs4 import BeautifulSoup
 
 
@@ -9,8 +9,19 @@ class Weather:
         self.key = 'ed968a36266723f284f2caeace939fba'
         self.lat = lat
         self.long = long
+        self.icon_dict = {'clear-day': 'clear.png',
+                          'clear-night': 'clear.png',
+                          'rain': 'rain.png',
+                          'snow': 'snow.png',
+                          'sleet': 'snow.png',
+                          'wind': 'cloudy.png',  # Change icon
+                          'fog': 'cloudy.png',  # Change icon
+                          'cloudy': 'cloudy.png',
+                          'partly-cloudy-day': 'partly_cloudy.png',
+                          'partly-cloudy-night': 'partly_cloudy.png'}  # Add night
 
     def get_current_weather(self):
+        curr_hour = datetime.now().hour
         forecast = forecastio.load_forecast(self.key, self.lat, self.long, units='si')
         curr_weather = forecast.currently()
         curr_temp = curr_weather.temperature
@@ -18,9 +29,17 @@ class Weather:
         curr_icon = curr_weather.icon
         print curr_summ
         print 'Temperature: %.2f C' % curr_temp
+        temps = [curr_temp]
+        icons = [curr_icon]
+        for i in range(4):
+            data_point = forecast.hourly().data[6*i+2]
+            temps.append(data_point.temperature)
+            icons.append(data_point.icon)
         filehandle = open("pepper_html/weather/weather.html")
         soup = BeautifulSoup(filehandle, 'html.parser')
-        soup.input.attrs['src'] = 'images/cloudy.png'
+        images = soup.find_all('input')
+        for idx, img in enumerate(images):
+            img['src'] = 'images/' + self.icon_dict[icons[idx]]
         print soup.prettify()
         with open("pepper_html/weather/output1.html", "w") as file:
             file.write(str(soup))
