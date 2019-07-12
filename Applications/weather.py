48653793c1f925d889c997d6f29aa9db46a1c493
AdamLiberda <adam.liberda@ericsson.com>
2019-07-12 11:24:36 +0200

    Cleanup + comments

--- Applications/weather.py
+++ Applications/weather.py
@@ -1,5 +1,5 @@
 import forecastio
-from datetime import date, timedelta, datetime
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 
@@ -23,23 +23,31 @@ class Weather:
     def get_current_weather(self):
         curr_hour = datetime.now().hour
         forecast = forecastio.load_forecast(self.key, self.lat, self.long, units='si')
+
         curr_weather = forecast.currently()
         curr_temp = curr_weather.temperature
         curr_summ = curr_weather.summary
         curr_icon = curr_weather.icon
+
         temps = [curr_temp]
         icons = [curr_icon]
         summ = [curr_summ]
         hours = [curr_hour+2, curr_hour+8, curr_hour+14, curr_hour+20]
-        hours = [str(i%24) + ':00' for i in hours]
+        hours = [str(i % 24) + ':00' for i in hours]
         hours.insert(0, 'NOW')
+
         for i in range(4):
+            # Get future forecasts 2hr from now and every 6th hour onwards
             data_point = forecast.hourly().data[6*i+2]
             temps.append(data_point.temperature)
             icons.append(data_point.icon)
             summ.append(data_point.summary)
+
+        # Get template file
         filehandle = open("pepper_html/weather/weather.html")
         soup = BeautifulSoup(filehandle, 'html.parser')
+
+        # Some ugly html coding
         images = soup.find_all('input')
         texts = soup.find_all('b')
         for idx in range(len(images)):
@@ -49,5 +57,6 @@ class Weather:
             temp_string = str(temps[idx].__format__('.1f')) + ' deg'
             texts[3*idx+2].string = temp_string
 
+        # Save file
         with open("pepper_html/weather/weather_test.html", "w") as file:
-            file.write(str(soup))
+            file.write(str(soup.prettify()))
